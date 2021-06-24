8d542a6d83f5c465f3ebb460d0ad4f620207d7c5
Arifuzzaman-Nishan <arifuzzamannishan@gmail.com>
2021-06-25 01:50:52 +0600

    navbar responsive done

--- src/Component/NavigationBar/NavigationBar.js
+++ src/Component/NavigationBar/NavigationBar.js
@@ -3,23 +3,23 @@ import homeIcon from '../../images/wanderon-logo.svg';
 import './NavigationBar.css';
 import Drawer from './SideBar';
 
-const NavigationBar = () => (
-    <div className="bg-danger nav-font">
-        <ul className="list-unstyled border border-dark d-flex align-items-center">
-            <li className="list-inline-item ml-5">
-                <img style={{ width: '5rem', color: 'red' }} src={homeIcon} alt="" />
-            </li>
-            <li className="m-auto">+091-9870301533</li>
-            <div className="ml-auto">
-                <li className="list-inline-item mr-5">TRENDINGTRIPS</li>
-                <li className="list-inline-item mr-5">WORKCATIONS</li>
-                <li className="list-inline-item mr-5">BLOGS</li>
-                <li className="list-inline-item mr-5 pr-5">
-                    <Drawer />
+export default function NavigationBar() {
+    return (
+        <div className="bg-danger nav-font">
+            <ul className="list-unstyled border border-dark d-flex align-items-center">
+                <li className="list-inline-item ml-3">
+                    <img style={{ width: '5rem', color: 'red' }} src={homeIcon} alt="" />
                 </li>
-            </div>
-        </ul>
-    </div>
-);
-
-export default NavigationBar;
+                <li className="m-auto">+091-9870301533</li>
+                <div className="ml-auto">
+                    <li className="list-inline-item mr-5 d-none d-md-inline">TRENDINGTRIPS</li>
+                    <li className="list-inline-item mr-5 d-none d-md-inline">WORKCATIONS</li>
+                    <li className="list-inline-item mr-5 d-none d-md-inline">BLOGS</li>
+                    <li className="list-inline-item mr-3 pr-2 d-md-none d-block">
+                        <Drawer />
+                    </li>
+                </div>
+            </ul>
+        </div>
+    );
+}
